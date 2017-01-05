1622903fa0586d7a6187d385e9ec8cec8c0deb81
Thomp <thomp@protonmail.com>
2017-01-04 20:21:24 -0600

    update values for pepperoni, hh mix, milk, lunchmeat

--- foodcosts.py
+++ foodcosts.py
@@ -44,8 +44,8 @@ hhmix = Food('hamburger helper mix', 1.5, 'box', 1)
 wheat_bread = Food('wheat bread', 1.2, 'slice', 20)
 raisinbread = Food('raisin bread', 2, 'slice', 16)
 cheese = Food('cheese', 2.5, 'slice', 11)
-lunchmeat = Food('lunchmeat', 5.99, 'slice', 24)
-pepperoni = Food('pepperoni', 4, 'slice', 30)
+lunchmeat = Food('lunchmeat', 5.99, 'slice', 32)
+pepperoni = Food('pepperoni', 4, 'slice', 70)
 
 
 wheat_sandwich = Meal('wheat sandwich')
@@ -55,7 +55,7 @@ raisinbread_sandwich = Meal('raisin bread sandwich')
 raisinbread_sandwich.ingredients = {'raisin bread': 4, 'cheese': 2, 'lunchmeat': 4}
 
 hh = Meal('hamburger helper')
-hh.ingredients = {'ground beef': .25, 'milk': .25, 'hamburger helper mix': .25}
+hh.ingredients = {'ground beef': 1/5, 'milk': 2/5, 'hamburger helper mix': 1/5}
 
 grilled_cheese = Meal('grilled cheese')
 grilled_cheese.ingredients = {'wheat bread': 4, 'cheese': 2, 'pepperoni': 6}
